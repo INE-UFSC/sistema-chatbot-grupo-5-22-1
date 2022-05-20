3530f7189cc092333284ba7bd255b8fa3e463c66
Lívia <livia@email.com>
2022-05-20 10:17:52 -0300

    versao final

--- app.py
+++ app.py
@@ -2,9 +2,11 @@
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
 from Bots.BotFeliz import BotFeliz
+from Bots.BotEspanhol import BotEspanhol
+from Bots.BotEspelhado import BotEspelhado
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Yoda"), BotFeliz("Julio")]
+lista_bots = [BotZangado("Yoda"), BotFeliz("Julio"), BotEspanhol("Joao"), BotEspelhado("Odahlepse")]
 
 sys_bot = scb.SistemaChatBot('Grupo 5', lista_bots)
 sys_bot.inicio()
